e1de30f4af832c8f0b7891375b7a3510b1084c26
Abhihsek Pankar <abhishek.pankar11@gmail.com>
2023-01-14 12:06:41 +0530

    ocr response message changed and logger removed

--- src/controllers/ocr.controller.ts
+++ src/controllers/ocr.controller.ts
@@ -3,14 +3,10 @@ import Tesseract from "tesseract.js";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const text = await Tesseract.recognize(req.body.url, 'eng', {
-            logger: console.log,
-        });
+        const data = await Tesseract.recognize(req.body.url, 'eng');
         return res.send({
-            message: 'Pattern saved in text file',
-            data: {
-                text: text.data.text.replace(/\n/g, ' '),
-            }
+            message: 'Data extracted',
+            data: data.data.text.replace(/\n/g, ' ')
         });
     } catch (error) {
         return next(error);
